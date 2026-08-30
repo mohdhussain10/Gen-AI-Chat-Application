74b6199d56de6a231471f5bedfc5505aeb343b35
mohdhussain10 <mohdhussain.rizvi10@gmail.com>
2026-08-30 22:44:47 +0330

    Commiting :
    Added Azure Open AI config in Docker-compose.yml file to read from it

--- GenAIChat/Program.cs
+++ GenAIChat/Program.cs
@@ -1,7 +1,10 @@
+using Azure.AI.OpenAI;
+using Azure.Identity;
 using GenAIChat.Interfaces;
 using GenAIChat.Services;
 using Microsoft.Extensions.AI;
 using OllamaSharp;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,23 +17,44 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+#region Ollama 
 //builder.Services.AddChatClient(
 //    new OllamaApiClient(
 //        new Uri("http://localhost:11434"),
 //        "qwen3:0.6b"));
 
-var ollamaEndpoint =
-    builder.Configuration["Ollama:Endpoint"]
+//var ollamaEndpoint =
+//    builder.Configuration["Ollama:Endpoint"]
+//    ?? "http://localhost:11434";
+
+//builder.Services
+//    .AddChatClient(
+//        new OllamaApiClient(
+//            new Uri(ollamaEndpoint)))
+//    .ConfigureOptions(options =>
+//    {
+//        options.ModelId = "qwen3:0.6b";
+//    });
+
+#endregion
+
+#region Azure Open AI
+var endpoint =
+    builder.Configuration["AzureOpenAI:Endpoint"]
     ?? "http://localhost:11434";
 
-builder.Services
-    .AddChatClient(
-        new OllamaApiClient(
-            new Uri(ollamaEndpoint)))
-    .ConfigureOptions(options =>
-    {
-        options.ModelId = "qwen3:0.6b";
-    });
+var deployementModelName = builder.Configuration["AzureOpenAI:Deployment"]
+    ?? "GenAIGPT";
+
+IChatClient chatClient =
+    new AzureOpenAIClient(
+        new Uri(endpoint),
+        new DefaultAzureCredential())
+    .GetChatClient(deployementModelName)
+    .AsIChatClient();
+
+#endregion
 
 //Registering services
 builder.Services.AddScoped<IChatService, ChatService>();
